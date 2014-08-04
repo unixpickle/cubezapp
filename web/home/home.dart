f22699b98a69e2d3086da5dd87de63c55c4c2ec0
Alex Nichol <unixpickle@gmail.com>
2014-08-04 18:49:12 -0400

    supports text entry

--- web/home/home.dart
+++ web/home/home.dart
@@ -5,6 +5,11 @@ import 'dart:async';
 
 part 'time_input.dart';
 
+TimeInput timeInput;
+
 void main() {
-  
+  timeInput = new TimeInput(querySelector('#time-input'));
+  timeInput.onSubmit.listen((KeyboardEvent e) {
+    window.alert('submitted time ${timeInput.time}');
+  });
 }
--- web/home/time_input.dart
+++ web/home/time_input.dart
@@ -1,55 +1,93 @@
 part of timer_page;
 
-class TimerInput {
+class TimeInput {
   final TextInputElement input;
-  String text;
+  final List<int> numbers;
   
-  TimerInput(this.input) {
-    input.onFocus.listen(handleFocus);
-    input.onKeyPress.listen(handleKeyPress);
-    text = '';
+  final List<StreamSubscription> _subs;
+  final StreamController<KeyboardEvent> _onSubmitController;
+  
+  Stream<KeyboardEvent> get onSubmit => _onSubmitController.stream;
+  
+  double get time {
+    assert(numbers.length <= 7);
+    List<double> fractions = [0.01, 0.1, 1.0, 10.0, 60.0, 600.0, 3600.0];
+    double result = 0.0;
+    for (int i = 0; i < numbers.length; i++) {
+      result += fractions[i] * _digitAt(i); 
+    }
+    return result;
   }
   
-  void handleFocus(Event e) {
-    e.preventDefault();
-    scheduleMicrotask(() {
-      input.selectionStart = input.value.length;
-      input.selectionEnd = input.value.length;
-    });
+  void set time(double t) {
+    int number = (t * 100.0).round();
+    List<int> moduli = [10, 10, 10, 6, 10, 6, 10];
+    numbers.clear();
+    for (int i = 0; i < 7 && number > 0; i++) {
+      numbers.insert(0, number % moduli[i]);
+      number ~/= moduli[i];
+    }
+    _updateInputValue();
   }
   
-  void handleKeyPress(KeyboardEvent event) {
+  TimeInput(this.input) : numbers = <int>[], _subs = <StreamSubscription>[],
+      _onSubmitController = new StreamController<KeyboardEvent>.broadcast() {
+    _subs.add(input.onFocus.listen(_positionCursor));
+    _subs.add(input.onKeyUp.listen(_positionCursor));
+    _subs.add(input.onMouseUp.listen(_positionCursor));
+    _subs.add(input.onKeyPress.listen(_handleKeyPress));
+    _subs.add(input.onKeyDown.listen(_handleKeyDown));
+  }
+  
+  void detach() {
+    for (StreamSubscription sub in _subs) {
+      sub.cancel();
+    }
+    _subs.clear();
+  }
+  
+  void _positionCursor(Event e) {
+    input.selectionStart = input.value.length;
+    input.selectionEnd = input.value.length;
+  }
+  
+  void _handleKeyPress(KeyboardEvent event) {
     event.preventDefault();
+    if (event.keyCode == 0xd || event.keyCode == 0x20) {
+      _onSubmitController.add(event);
+      return;
+    }
     
+    if (numbers.length > 6) return;
     if (event.keyCode < 0x30 || event.keyCode > 0x39) {
-      
-    }
-    int idx = input.selectionStart - 1;
-    if (idx < 0 || idx >= input.value.length) {
       return;
     }
-    
-    String replacing = input.value[idx];
-    if (replacing == ':') {
-      if (event.keyCode != 0x3a) return;
-    } else if (replacing == '.') {
-      if (event.keyCode != 0x2e) return;
-    } else if (event.keyCode < 0x30 || event.keyCode > 0x39) {
-      return;
-    }
-    String pref = input.value.substring(0, idx);
-    String end = input.value.substring(idx + 1);
-    String key = new String.fromCharCode(event.keyCode);
-    input.value = '$pref$key$end';
-    input.selectionEnd = idx - 1;
+    numbers.add(event.keyCode - 0x30);
+    _updateInputValue();
   }
   
-  void updateInputValue() {
-    String realText = text;
-    while (realText.length < 5) {
-      realText = realText + '0';
-    } 
-    input.value = realText[4] + ':' + realText.substring(2, 4) + '.' +
-        realText.substring(0, 2);
+  void _handleKeyDown(KeyboardEvent event) {
+    if (event.keyCode == 8) {
+      event.preventDefault();
+      if (numbers.length > 0) {
+        numbers.removeLast();
+        _updateInputValue();
+      }
+    }
+  }
+  
+  void _updateInputValue() {
+    String str = '${_digitAt(5)}${_digitAt(4)}:${_digitAt(3)}${_digitAt(2)}.' +
+        '${_digitAt(1)}${_digitAt(0)}';
+    for (int i = 6; i < numbers.length; i++) {
+      if (i == 6) str = ':' + str;
+      str = '${_digitAt(i)}$str';
+    }
+    input.value = str;
+  }
+  
+  int _digitAt(int idx) {
+    if (idx >= numbers.length) return 0;
+    return numbers[numbers.length - idx - 1];
   }
 }
