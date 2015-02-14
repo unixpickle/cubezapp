658ee822d8eaf8aa7e643c8463104c18a585c852
Alex Nichol <unixpickle@gmail.com>
2015-02-14 14:44:16 -0500

    switch to settings tab

--- assets/scripts/view/footer.js
+++ assets/scripts/view/footer.js
@@ -3,6 +3,9 @@
   function Footer() {
     this.element = $('#footer');
     this.open = !(localStorage.footerOpen === 'false');
+    this.statsTab = this.element.find('#stats-tab');
+    this.settingsTab = this.element.find('#settings-tab');
+    this.currentTab = 0;
     
     if (!this.open) {
       this.element.css({'bottom': -256});
@@ -10,7 +13,32 @@
     
     var closeButton = this.element.find('#footer-close');
     closeButton.click(this.toggleOpen.bind(this));
+    
+    $('#stats-tab-button').click(this.showStatsTab.bind(this));
+    $('#settings-tab-button').click(this.showSettingsTab.bind(this));
   }
+  
+  Footer.prototype.showSettingsTab = function() {
+    if (this.currentTab === 1) {
+      return;
+    }
+    this.currentTab = 1;
+    $('#stats-tab-button').attr('class', 'tab');
+    $('#settings-tab-button').attr('class', 'tab-selected');
+    this.settingsTab.css({display: 'block'});
+    this.statsTab.css({display: 'none'});
+  };
+  
+  Footer.prototype.showStatsTab = function() {
+    if (this.currentTab === 0) {
+      return;
+    }
+    this.currentTab = 0;
+    $('#stats-tab-button').attr('class', 'tab-selected');
+    $('#settings-tab-button').attr('class', 'tab');
+    this.settingsTab.css({display: 'none'});
+    this.statsTab.css({display: 'block'});
+  };
   
   Footer.prototype.toggleOpen = function() {
     if (this.open) {
