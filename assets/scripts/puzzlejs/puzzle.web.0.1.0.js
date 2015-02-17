25908766e83e49b1a0345f09cd77cd621255db1e
Alex Nichol <unixpickle@gmail.com>
2015-02-17 15:22:22 -0500

    use puzzle.js v0.1.0

--- assets/scripts/puzzlejs/puzzle.web.0.1.0.js
+++ assets/scripts/puzzlejs/puzzle.web.0.1.0.js
@@ -1949,12 +1949,82 @@
   exports.scramblersForPuzzle = scramblersForPuzzle;
 
 })();
-self.onmessage = function(e) {
-  var m = e.data;
-  var puzzle = m.puzzle;
-  var scrambler = m.scrambler;
-  var moves = m.moves;
-  var scramble = self.puzzlejs.scrambler.generateScramble(puzzle, scrambler,
-    moves);
-  self.postMessage({id: m.id, scramble: scramble});
-};
+(function() {
+
+  // Uncomment the following line and put in the webworker path if necessary.
+  // var workerPath = 'puzzlejs/webscrambler_worker.js';
+  
+  var workerPath = null;
+
+  var scrambleWorker = null;
+  var workerUnavailable = false;
+  var callbacks = {};
+  var ticketId = 0;
+
+  if ('undefined' !== typeof window.Worker) {
+    // We may need to find the worker's path manually.
+    if (workerPath === null) {
+      // Use the current script's "src" attribute to figure out where the
+      // scripts are.
+      var scripts = document.getElementsByTagName('script');
+      if (scripts.length === 0) {
+        throw new Error('unable to find worker path');
+      }
+      var scriptPath = scripts[scripts.length-1].src.split('?')[0];
+      var slashIdx = scriptPath.lastIndexOf('/');
+      if (slashIdx >= 0) {
+        workerPath = scriptPath.slice(0, slashIdx) + '/webscrambler_worker.js';
+      } else {
+        workerPath = 'webscrambler_worker.js';
+      }
+    }
+  }
+
+  function generateScramble(puzzle, scrambler, moves, cb) {
+    if (scrambleWorker === null) {
+      if ('undefined' === typeof window.Worker || workerUnavailable) {
+        // No WebWorker support; generate scramble on main thread.
+        setTimeout(function() {
+          var genScram = window.puzzlejs.scrambler.generateScramble;
+          var res = genScram(puzzle, scrambler, moves);
+          cb(res);
+        }, 10);
+        return;
+      }
+      
+      // Create the WebWorker.
+      try {
+        setupWorker();
+      } catch (e) {
+        workerUnavailable = true;
+        generateScramble(puzzle, scrambler, moves, cb);
+        return;
+      }
+    }
+
+    // Send a request to the WebWorker.
+    var ticket = ticketId++;
+    callbacks[ticket] = cb;
+    scrambleWorker.postMessage({puzzle: puzzle, scrambler: scrambler,
+      moves: moves, id: ticket});
+  }
+  
+  function setupWorker() {
+    // Setup the webworker to call our callbacks.
+    scrambleWorker = new window.Worker(workerPath);
+    scrambleWorker.onmessage = function(e) {
+      var m = e.data;
+      var cb = callbacks[m.id]
+      delete callbacks[m.id];
+      cb(m.scramble);
+    }
+  }
+
+  if (!window.puzzlejs) {
+    window.puzzlejs = {webscrambler: {}};
+  } else if (!window.puzzlejs.webscrambler) {
+    window.puzzlejs.webscrambler = {};
+  }
+  window.puzzlejs.webscrambler.generateScramble = generateScramble;
+
+})();
