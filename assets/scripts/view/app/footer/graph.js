d6c660137f0ea3dcc2cf3224ac38cb1e530f5c77
Alex Nichol <unixpickle@gmail.com>
2015-05-27 21:24:03 -0400

    cleanup whitespace

--- assets/scripts/view/app/footer/graph.js
+++ assets/scripts/view/app/footer/graph.js
@@ -1,5 +1,5 @@
 (function() {
-  
+
   var SETTINGS_BACKGROUND = '#e6e6e6';
   var SETTINGS_HEADER_HEIGHT = 40;
   var SETTINGS_WIDTH = 170;
@@ -7,7 +7,7 @@
   function Graph() {
     this._$element = $('#graph');
     this._settings = new GraphSettings();
-    
+
     // For now, we show the settings all the time for debugging.
     this._settings.element().css({position: 'absolute', right: 0});
     this._$element.append(this._settings.element());
@@ -20,7 +20,7 @@
   Graph.prototype.setVisible = function(flag) {
     this._$element.css({display: flag ? 'block' : 'none'});
   };
-  
+
   function GraphSettings() {
     this._$element = $('<div></div>').css({
       height: '100%',
@@ -28,7 +28,7 @@
       backgroundColor: SETTINGS_BACKGROUND,
       position: 'absolute'
     });
-    
+
     this._$header = $('<div></div>').addClass('flavor-background').css({
       position: 'absolute',
       top: 0,
@@ -36,10 +36,10 @@
       height: SETTINGS_HEADER_HEIGHT,
       width: '100%'
     });
-    
+
     this._$element.append(this._$header);
   }
-  
+
   GraphSettings.prototype.element = function() {
     return this._$element;
   };
