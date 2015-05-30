abdd80d12b1b11f413dd1f9de324eb66effe7b7c
Alex Nichol <unixpickle@gmail.com>
2015-05-29 21:19:04 -0400

    attempted to show dropdown on graph settings

--- assets/scripts/view/app/footer/graph.js
+++ assets/scripts/view/app/footer/graph.js
@@ -20,13 +20,29 @@
   function GraphSettings() {
     this._$element = $('<div id="graph-settings"></div>');
     this._$header = $('<div class="title flavor-background"></div>');
-    this._$modeLabel = $('<label></label>');
 
+    this._$modeLabel = $('<label></label>');
+    this._$modeLabel.click(this._modeLabelClicked.bind(this));
+
+    this._$modeDropdown = $('<ul class="mode-dropdown"></ul>').css({
+      display: 'none'
+    });
+    for (var i = 0, len = GraphSettings.MODES.length; i < len; ++i) {
+      this._$modeDropdown.append($('<li></li>').text(GraphSettings.MODES[i]));
+    }
+    this._showingDropdown = false;
+
+    // TODO: switch to the page the user was last on.
     this._$modeLabel.text('Standard');
+
+    // NOTE: we need to add the dropdown after the header so the shadow of the
+    // dropdown is covered.
     this._$header.append(this._$modeLabel);
-    this._$element.append(this._$header);
+    this._$element.append(this._$modeDropdown).append(this._$header);
   }
 
+  GraphSettings.ANIMATION_DURATION = 150;
+  GraphSettings.MODES = ['Standard', 'Mean', 'Histogram', 'Streak'];
   GraphSettings.MODE_STANDARD = 'Standard';
   GraphSettings.MODE_MEAN = 'Mean';
   GraphSettings.MODE_HISTOGRAM = 'Histogram';
@@ -35,6 +51,22 @@
   GraphSettings.prototype.element = function() {
     return this._$element;
   };
+
+  GraphSettings.prototype._modeLabelClicked = function() {
+    this._setShowingDropdown(!this._showingDropdown);
+  };
+
+  GraphSettings.prototype._setShowingDropdown = function(x) {
+    if (x == this._showingDropdown) {
+      return;
+    }
+    this._showingDropdown = x;
+    if (x) {
+      this._$modeDropdown.stop(true, true).css({display: 'block', opacity: 1});
+    } else {
+      this._$modeDropdown.fadeOut(GraphSettings.ANIMATION_DURATION);
+    }
+  }
 
   window.app.Graph = Graph;
 
