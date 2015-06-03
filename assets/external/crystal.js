d671b04e956d79766ec563ccaf764d7887c559b1
Alex Nichol <unixpickle@gmail.com>
2015-06-03 10:33:47 -0400

    cleanup whitespace in crystal.js

--- assets/external/crystal.js
+++ assets/external/crystal.js
@@ -1,37 +1,37 @@
 (function() {
-  
+
   var currentRatio = calculateRatio();
   var listeners = [];
-  
+
   function addListener(f) {
     listeners.push(f);
   }
-  
+
   function calculateRatio() {
     // TODO: here, do something to detect browser zoom.
     return window.devicePixelRatio || 1;
   }
-  
+
   function getRatio() {
     return currentRatio;
   }
-  
+
   function removeListener(f) {
     var idx = listeners.indexOf(f);
     if (idx >= 0) {
       listeners.splice(idx, 1);
     }
   }
-  
+
   function update() {
     var rat = calculateRatio();
     if (rat === currentRatio) {
       return;
     }
-    
+
     // The ratio has changed.
     currentRatio = rat;
-    
+
     // Call each listener.
     // NOTE: we copy the listeners because any given listener could remove/add
     // listeners as it pleases.
@@ -40,13 +40,13 @@
       theListeners[i]();
     }
   }
-  
+
   window.crystal = {
     addListener: addListener,
     getRatio: getRatio,
     removeListener: removeListener
   };
-  
+
   setInterval(update, 250);
-  
+
 })();
