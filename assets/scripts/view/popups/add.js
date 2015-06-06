f58fd3e80b8d5a56348e9bcf3047cab1580ae37e
Alex Nichol <unixpickle@gmail.com>
2015-06-06 14:06:42 -0400

    drafted new popup mechanism; controller broken

--- assets/scripts/view/popups/add.js
+++ assets/scripts/view/popups/add.js
@@ -2,53 +2,42 @@
 
   var FIELD_WIDTH = 160;
   var FIELD_HEIGHT = 30;
-  var CONTENT_HEIGHT = 200;
 
   function AddPopup() {
     window.app.EventEmitter.call(this);
 
-    var $puzzle = $('<div class="puzzle"></div>');
-    this._$puzzleIcon = $('<div class="icon flavor-background"></div>');
-    this._$puzzleName = $('<label>Name</label>');
-    $puzzle.append([this._$puzzleIcon, this._$puzzleName]);
-
-    this._$fields = $('<div class="fields"></div>');
-
-    // This state is used to know whether or not to play an animation to show
-    // the subscramble dropdown.
-    this._showingSubscramble = false;
-
-    // Keep track of which field the user has explicitly set.
-    this._userChangedIcon = false;
-    this._userChangedScramble = false;
-
-    // Create the rest of the hidden class.
-    this._dialog = null;
-    this._$bldField = null;
-    this._bldCheck = null;
-    this._$iconField = null;
-    this._iconDropdown = null;
-    this._$nameField = null;
-    this._$nameInput = null;
-    this._$scrambleField = null;
-    this._scrambleDropdown = null;
-    this._$subscrambleField = null;
-    this._subscrambleDropdown = null;
-
-    this._createBLDField();
-    this._createIconField();
-    this._createNameField();
-    this._createScrambleField();
-    this._createSubscrambleField();
-
-    this._initialLayout();
+    this._viewModel = new ViewModel();
 
     var $element = $('<div class="add-popup-content"></div>');
-    var $separator = $('<div class="separator"></div>');
-    $element.append([$puzzle, $separator, this._$fields]);
+
+    var $puzzle = $('<div class="add-popup-puzzle"></div>');
+    this._$puzzleIcon = $('<div class="flavor-background ' +
+      'add-popup-puzzle-icon"></div>');
+    this._$puzzleName = $('<label class="add-popup-puzzle-label"></label>');
+    $puzzle.append(this._$puzzleIcon, this._$puzzleName);
+    $element.append($puzzle);
+
+    this._$fieldContainer = $('<div class="add-popup-fields"></div>');
+    $element.append(this._$fieldContainer);
+
+    this._fields = {};
+    this._addField('name', 'Name', createNameField());
+    this._addField('icon', 'Icon', createDropdown(window.app.iconNames,
+      window.app.iconNames.indexOf('3x3x3')));
+    this._addField('scrambler', 'Scramble', createDropdown(scrambleNames(), 0));
+    this._addField('scrambleType', '', createDropdown());
+    this._addField('bld', 'BLD', window.app.flavors.makeCheckbox());
+
+    var $separator = $('<div class="add-popup-separator"></div>');
+    $element.append($separator);
+
     this._dialog = new window.app.Dialog('New Puzzle', $element, ['Create']);
     this._dialog.on('action', this.emit.bind(this, 'create'));
     this._dialog.on('close', this._handleClose.bind(this));
+
+    this._registerUIEvents();
+    this._registerViewModelEvents();
+    this._viewModel.emitCurrentValues();
   }
 
   AddPopup.prototype = Object.create(window.app.EventEmitter.prototype);
@@ -58,260 +47,238 @@
     this._handleClose();
   };
 
-  AddPopup.prototype.getBLD = function() {
-    return this._bldCheck.getChecked();
-  };
-
-  AddPopup.prototype.getIcon = function() {
-    return window.app.iconFiles[this._iconDropdown.getSelected()];
-  };
-
-  AddPopup.prototype.getName = function() {
-    return this._$nameInput.val();
-  };
-
-  AddPopup.prototype.getScrambleType = function() {
-    var subscramblers = this._subscramblers();
-    if (subscramblers.length === 1) {
-      return subscramblers[0];
-    } else if (subscramblers.length > 1) {
-      return this._subscrambleDropdown.getValue();
-    }
-  };
-
-  AddPopup.prototype.getScrambler = function() {
-    return this._scrambleDropdown.getValue();
-  };
-
   AddPopup.prototype.shakeName = function() {
-    this._$nameInput.focus();
-    window.app.runShakeAnimation(this._$nameInput[0]);
+    var $nameInput = this._getField('name');
+    $nameInput.focus();
+    window.app.runShakeAnimation($nameInput[0]);
   };
 
   AddPopup.prototype.show = function() {
     this._dialog.show();
   };
 
-  AddPopup.prototype._changedIcon = function() {
-    this._userChangedIcon = true;
-
-    this._$puzzleIcon.css({
-      backgroundImage: 'url(images/puzzles/' + this.getIcon() + '.png)'
-    });
-
-    // If we can change the scramble, try it. There's no harm in it.
-    if (!this._userChangedScramble) {
-      var iconName = this._iconDropdown.getValue();
-      var lastIdx = this._scrambleDropdown.getSelected();
-      this._scrambleDropdown.setValue(iconName);
-      if (this._scrambleDropdown.getSelected() !== lastIdx) {
-        this._changedScramble();
-        this._userChangedScramble = false;
-      }
-    }
+  AddPopup.prototype.viewModel = function() {
+    return this._viewModel;
   };
 
-  AddPopup.prototype._changedName = function() {
-    this._$puzzleName.text(this._$nameInput.val() || 'Name');
-    if (!this._userChangedIcon) {
-      this._iconDropdown.setValue(this._$nameInput.val());
-      if (this._iconDropdown.getValue() === this._$nameInput.val()) {
-        this._changedIcon();
-        this._userChangedIcon = false;
-      }
-    }
+  AddPopup.prototype._addField = function(rawName, name, input) {
+    var field = new Field(rawName, name, input);
+    this._fields[rawName] = field.input();
+    
+    // We use prepend to make sure the z order is correct.
+    this._$fieldContainer.prepend(field.element());
   };
 
-  AddPopup.prototype._changedScramble = function() {
-    this._userChangedScramble = true;
-
-    var subScramblers = this._subscramblers();
-    var showSub = (subScramblers.length > 1);
-
-    // Update the subscramblers in the dropdown if possible.
-    if (showSub) {
-      this._subscrambleDropdown.setOptions(subScramblers, 0);
-    }
-
-    // If the subscramble visibility did not change, no animation or relayout is
-    // needed.
-    if (showSub === this._showingSubscramble) {
-      return;
-    }
-
-    this._showingSubscramble = showSub;
-
-    // Animate all elements to their new positions.
-    var positions = this._fieldPositions();
-    var fields = [this._$nameField, this._$iconField, this._$scrambleField,
-      this._$bldField];
-    for (var i = 0; i < 4; ++i) {
-      var y = positions[i];
-      fields[i].animate({top: y});
-    }
-
-    // Fade in/out the subscramble field.
-    if (showSub) {
-      this._$subscrambleField.fadeIn();
-    } else {
-      this._$subscrambleField.fadeOut();
-    }
+  AddPopup.prototype._getField = function(rawName) {
+    return this._fields[rawName];
   };
 
-  AddPopup.prototype._changedSubscramble = function() {
-    this._userChangedScramble = true;
-  };
-
-  AddPopup.prototype._createBLDField = function() {
-    this._$bldField = $('\
-      <div class="field"> \
-        <label>BLD</label> \
-        <div class="content"></div> \
-      </div> \
-    ');
-    var content = this._$bldField.find('.content');
-    this._bldCheck = window.app.flavors.makeCheckbox();
-    content.append(this._bldCheck.element());
-  };
-
-  AddPopup.prototype._createIconField = function() {
-    // Generate the dropdown field.
-    var res = createDropdownField('Icon', window.app.iconNames,
-      window.app.iconNames.indexOf('3x3x3'));
-    this._iconDropdown = res.dropdown;
-    this._$iconField = res.field;
-
-    // Changing the icon field changes the icon in the preview.
-    this._iconDropdown.onChange = this._changedIcon.bind(this);
-  };
-
-  AddPopup.prototype._createNameField = function() {
-    this._$nameField = $('\
-      <div class="field"> \
-        <label>Name</label> \
-        <div class="content"> \
-          <input placeholder="Name" maxlength="19"> \
-        </div> \
-      </div> \
-    ');
-    this._$nameInput = this._$nameField.find('input');
-
-    // When the text changes, change the puzzle name on the left.
-    this._$nameInput.keydown(function() {
-      // The text isn't changed by the keydown, so we wait 10ms.
-      setTimeout(function() {
-        this._changedName();
-      }.bind(this), 10);
-    }.bind(this));
-    this._$nameInput.change(this._changedName.bind(this));
-  };
-
-  AddPopup.prototype._createScrambleField = function() {
-    // Generate the puzzles.
-    var puzzles = window.puzzlejs.scrambler.allPuzzles();
-    puzzles.unshift('None');
-
-    // Generate the dropdown field.
-    var res = createDropdownField('Scramble', puzzles, 0);
-    this._scrambleDropdown = res.dropdown;
-    this._$scrambleField = res.field;
-
-    // When the scramble changes, it changes the subscrambles.
-    this._scrambleDropdown.onChange = this._changedScramble.bind(this);
-  };
-
-  AddPopup.prototype._createSubscrambleField = function() {
-    var res = createDropdownField(null, null, 0);
-    this._subscrambleDropdown = res.dropdown;
-    this._$subscrambleField = res.field;
-
-    // By default, this field is invisible.
-    this._$subscrambleField.css({display: 'none'});
-    this._subscrambleDropdown.onChange = this._changedSubscramble.bind(this);
-  };
-
-  // _fieldPositions computes the top coordinates for each field.
-  AddPopup.prototype._fieldPositions = function() {
-    if (this._showingSubscramble) {
-      var contentHeight = FIELD_HEIGHT * 5;
-      var spacing = (CONTENT_HEIGHT-contentHeight) / 6;
-      var tops = [];
-      for (var i = 0; i < 5; ++i) {
-        if (i !== 3) {
-          tops.push(Math.round(spacing*(i+1) + FIELD_HEIGHT*i));
-        }
-      }
-      return tops;
-    } else {
-      var contentHeight = FIELD_HEIGHT * 4;
-      var spacing = (CONTENT_HEIGHT-contentHeight) / 5;
-      var tops = [];
-      for (var i = 0; i < 4; ++i) {
-        tops[i] = Math.round(spacing*(i+1) + FIELD_HEIGHT*i);
-      }
-      return tops;
-    }
-  };
-
-  // _handleClose makes sure no dropdowns are open.
+  // _handleClose cleans up the components of the popup after the user is done
+  // with it.
   AddPopup.prototype._handleClose = function() {
-    this._iconDropdown.close();
-    this._scrambleDropdown.close();
-    this._subscrambleDropdown.close();
-    window.app.flavors.removeCheckbox(this._bldCheck);
-  };
-
-  // _initialLayout puts the fields in their respective places.
-  AddPopup.prototype._initialLayout = function() {
-    // Compute the subscramble field's position.
-    var contentHeight = FIELD_HEIGHT * 5;
-    var spacing = (CONTENT_HEIGHT-contentHeight) / 6;
-    this._$subscrambleField.css({top: spacing*4 + FIELD_HEIGHT*3});
-
-    // Compute the position for the rest of the fields and append them.
-    var positions = this._fieldPositions();
-    var fields = [this._$nameField, this._$iconField, this._$scrambleField,
-      this._$bldField];
-    for (var i = 0; i < 4; ++i) {
-      fields[i].css({top: positions[i]});
-      this._$fields.append(fields[i]);
+    var dropdownNames = ['icon', 'scrambler', 'scrambleType']
+    for (var i = 0, len = dropdownNames.length; i < len; ++i) {
+      this._getField(dropdownNames[i]).close();
     }
-    this._$fields.append(this._$subscrambleField);
+    window.app.flavors.removeCheckbox(this._getField('bld'));
   };
 
-  AddPopup.prototype._subscramblers = function() {
-    var puzzle = this._scrambleDropdown.getValue();
-    if (puzzle === 'None') {
+  AddPopup.prototype._registerUIEvents = function() {
+    var $nameInput = this._getField('name');
+    var updateName = function() {
+      this._viewModel.setField('name', $nameInput.val());
+    }.bind(this);
+    // NOTE: the text isn't changed before the keydown, so we use setTimeout.
+    $nameInput.keydown(setTimeout.bind(null, updateName, 10));
+    $nameInput.change(updateName);
+
+    var scramblerDropdown = this._getField('scrambler');
+    scramblerDropdown.onChange = function() {
+      this._viewModel.setField('scrambler', scramblerDropdown.getValue());
+    }.bind(this);
+
+    var scrambleTypeDropdown = this._getField('scrambleType');
+    scrambleTypeDropdown.onChange = function() {
+      this._viewModel.setField('scrambleType', scrambleTypeDropdown.getValue());
+    }.bind(this);
+
+    var iconDropdown = this._getField('icon');
+    iconDropdown.onChange = function() {
+      var fileName = window.app.iconFiles[iconDropdown.getSelected()];
+      this._viewModel.setField('icon', fileName);
+    }.bind(this);
+
+    var bldCheck = this._getField('bld');
+    bldCheck.onChange = function() {
+      this._viewModel.setField('bld', bldCheck.getChecked());
+    }.bind(this);
+  };
+
+  AddPopup.prototype._registerViewModelEvents = function() {
+    this._viewModel.on('name', function(name) {
+      this._getField('name').val(name);
+      if (name.trim().length > 0) {
+        this._$puzzleName.text(name.trim());
+      } else {
+        this._$puzzleName.text('Name');
+      }
+    }.bind(this));
+
+    this._viewModel.on('scrambler', function(scrambler) {
+      this._getField('scrambler').setValue(scrambler);
+    }.bind(this));
+
+    this._viewModel.on('scrambleType', function(scrambleType) {
+      if (this._viewModel.showScrambleType()) {
+        this._getField('scrambleType').setValue(scrambleType);
+      }
+    }.bind(this));
+
+    this._viewModel.on('icon', function(iconFile) {
+      var iconName = window.app.iconFilesToNames[iconFile];
+      this._getField('icon').setValue(iconName);
+      this._$puzzleIcon.css({backgroundImage: 'url(images/puzzles/' +
+        iconFile + '.png)'});
+    }.bind(this));
+
+    this._viewModel.on('bld', function(flag) {
+      this._getField('bld').setChecked(flag);
+    }.bind(this));
+
+    this._viewModel.on('scrambleTypes', this._scrambleTypesChanged.bind(this));
+  };
+
+  AddPopup.prototype._scrambleTypesChanged = function() {
+    if (this._viewModel.showScrambleType()) {
+      var types = this._viewModel.scrambleTypes();
+      this._getField('scrambleType').setOptions(types, 0);
+      this._$fieldContainer.addClass('add-popup-fields-all');
+    } else {
+      this._$fieldContainer.removeClass('add-popup-fields-all');
+    }
+  };
+
+  function Field(rawName, name, input) {
+    this._input = input;
+    var $input = input;
+    if ('function' === typeof input.element) {
+      $input = input.element();
+    }
+
+    var $nameLabel = $('<label class="add-popup-label"></label>').text(name);
+    var $inputContainer = $('<div class="add-popup-input-container"></div>');
+    $inputContainer.append($input);
+    this._$element = $('<div class="add-popup-field"></div>').append(
+      $nameLabel, $inputContainer
+    ).addClass('add-popup-' + rawName + '-field');
+  }
+
+  Field.prototype.element = function() {
+    return this._$element;
+  };
+
+  Field.prototype.input = function() {
+    return this._input;
+  };
+
+  function ViewModel() {
+    window.app.EventEmitter.call(this);
+
+    this._fields = {
+      name: '',
+      icon: '3x3x3',
+      scrambler: 'None',
+      scrambleType: 'None',
+      bld: false
+    };
+
+    this._autocompleteFields = ViewModel.FIELD_NAMES.slice();
+  }
+
+  ViewModel.FIELD_NAMES = ['scrambler', 'scrambleType', 'icon', 'name', 'bld'];
+
+  ViewModel.prototype = Object.create(window.app.EventEmitter.prototype);
+
+  ViewModel.prototype.emitCurrentValues = function() {
+    for (var i = 0, len = ViewModel.FIELD_NAMES.length; i < len; ++i) {
+      var name = ViewModel.FIELD_NAMES[i];
+      this.emit(name, this.getField(name));
+    }
+  };
+
+  ViewModel.prototype.getField = function(name) {
+    return this._fields[name];
+  };
+
+  ViewModel.prototype.scrambleTypes = function() {
+    var scrambler = this.getField('scrambler');
+    if (scrambler === 'None') {
       return [];
     }
-
     var names = [];
-    var scramblers = window.puzzlejs.scrambler.scramblersForPuzzle(puzzle);
+    var scramblers = window.puzzlejs.scrambler.scramblersForPuzzle(scrambler);
     for (var i = 0, len = scramblers.length; i < len; ++i) {
       names[i] = scramblers[i].name;
     }
     return names;
   };
 
-  function createDropdownField(label, options, selected) {
-    // Create the element.
-    var element = $('<div class="field"></div>');
+  ViewModel.prototype.setField = function(name, value) {
+    this._removeAutocompleteField(name);
+    this._updateAndEmit(name, value);
+    this._autocomplete();
+  };
 
-    // Create the label.
-    if (label !== null) {
-      element.append($('<label><label>').text(label));
+  ViewModel.prototype.showScrambleType = function() {
+    var scrambler = this.getField('scrambler');
+    if (scrambler === 'None') {
+      return false;
     }
+    return window.puzzlejs.scrambler.scramblersForPuzzle(scrambler).length > 1;
+  };
 
-    // Create the content.
+  ViewModel.prototype._autocomplete = function() {
+    // TODO: this.
+  };
+
+  ViewModel.prototype._canAutocompleteField = function(name) {
+    return this._autocompleteFields.indexOf(name) >= 0;
+  };
+
+  ViewModel.prototype._removeAutocompleteField = function(name) {
+    var index = this._autocompleteFields.indexOf(name);
+    if (index >= 0) {
+      this._autocompleteFields.splice(index, 1);
+    }
+  };
+
+  ViewModel.prototype._updateAndEmit = function(name, value) {
+    if (this._fields[name] !== value) {
+      this._fields[name] = value;
+      this.emit(name, value);
+      if (name === 'scrambler') {
+        this.emit('scrambleTypes');
+        this._updateAndEmit('scrambleType', this.scrambleTypes()[0]);
+      }
+    }
+  };
+
+  function createDropdown(options, selected) {
     var dropdown = new window.dropdownjs.Dropdown(FIELD_WIDTH);
-    if (options !== null) {
+    if (options) {
       dropdown.setOptions(options, selected);
     }
-    var content = $('<div class="content"></div>');
-    content.append(dropdown.element());
-    element.append(content);
+    return dropdown;
+  }
 
-    return {field: element, dropdown: dropdown};
+  function createNameField() {
+    return $('<input class="add-popup-text-input" placeholder="Name">');
+  }
+
+  function scrambleNames() {
+    var names = window.puzzlejs.scrambler.allPuzzles();
+    names.unshift('None');
+    return names;
   }
 
   window.app.AddPopup = AddPopup;
