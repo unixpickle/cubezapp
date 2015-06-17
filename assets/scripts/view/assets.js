d5655aa9e93660e618e93d4613b7b28848da4831
Alex Nichol <unixpickle@gmail.com>
2015-06-17 19:48:57 -0400

    remove references to fox icon

--- assets/scripts/view/assets.js
+++ assets/scripts/view/assets.js
@@ -2,12 +2,12 @@
 
   window.app.iconFiles = [
     '2x2x2', '3x3x3', '4x4x4', '5x5x5', '6x6x6', '7x7x7', 'BLD', 'clock',
-    'feet', 'fox', 'megaminx', 'OH', 'pyraminx', 'skewb', 'square1'
+    'feet', 'megaminx', 'OH', 'pyraminx', 'skewb', 'square1'
   ];
 
   window.app.iconNames = [
     '2x2x2', '3x3x3', '4x4x4', '5x5x5', '6x6x6', '7x7x7', 'BLD',
-    "Rubik's Clock", 'Feet', 'Fox', 'Megaminx', 'OH', 'Pyraminx', 'Skewb',
+    "Rubik's Clock", 'Feet', 'Megaminx', 'OH', 'Pyraminx', 'Skewb',
     'Square-1'
   ];
 
