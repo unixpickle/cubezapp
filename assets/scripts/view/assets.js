c27d955a7d7aae4b9a9c1518f0290e928d1b4a1d
Alex Nichol <unixpickle@gmail.com>
2015-08-04 21:07:43 -0400

    added egg icon

--- assets/scripts/view/assets.js
+++ assets/scripts/view/assets.js
@@ -2,13 +2,13 @@
 
   window.app.iconFiles = [
     '2x2x2', '3x3x3', '4x4x4', '5x5x5', '6x6x6', '7x7x7', 'BLD', 'clock',
-    'feet', 'megaminx', 'OH', 'pyraminx', 'skewb', 'square1', 'mirror'
+    'feet', 'megaminx', 'OH', 'pyraminx', 'skewb', 'square1', 'mirror', 'egg'
   ];
 
   window.app.iconNames = [
     '2x2x2', '3x3x3', '4x4x4', '5x5x5', '6x6x6', '7x7x7', 'BLD',
     "Rubik's Clock", 'Feet', 'Megaminx', 'OH', 'Pyraminx', 'Skewb',
-    'Square-1', 'Mirror Cube'
+    'Square-1', 'Mirror Cube', 'Egg'
   ];
 
   window.app.iconFilesToNames = {
